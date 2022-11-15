d1f2103264c2c5b5ebe0052e902c411f776c13bb
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2022-11-15 19:37:50 +0530

    Create Water_Detector_v2.py

---
+++ IOT&ES/Water_Detector_v2.py
@@ -0,0 +1,17 @@
+imprt RPi.GPIO as GPIO
+
+GPIO_pin_in=5
+GPIO_pin_out=7
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(gpio_pin_in,IN)
+GPIO.setup(gpio_pin_out,OUT)
+
+while True:
+  v=GPIO.input(5)
+  
+  if(v==0):
+    GPIO.output(gpio_pin_out,True)
+  else:
+    GPIO.output(gpio_pin_out,False)
+    
